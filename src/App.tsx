ece38fbb01968545d00b59902946230459079fac
Ritchie Dimaria <rdimaria@Ritchies-MacBook-Air.local>
2024-05-24 17:12:55 -0400

    Add start script for Vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Button } from "./@/components/ui/button"
-import { Progress } from "./@/components/ui/progress"
+//import { Progress } from "./@/components/ui/progress"
 import { Input } from "./@/components/ui/input"
 
 const App = () => {
