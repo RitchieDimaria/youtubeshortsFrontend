437d06d877a4f71fc6c29d69436c5ce7828cabe5
Ritchie Dimaria <rdimaria@Ritchies-MacBook-Air.local>
2024-05-24 17:22:00 -0400

    Configured vite to expose server to network

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 export default defineConfig({
+  server: {
+    host: true,
+    port: 5173  },
   plugins: [react()],
   resolve: {
     alias: {
